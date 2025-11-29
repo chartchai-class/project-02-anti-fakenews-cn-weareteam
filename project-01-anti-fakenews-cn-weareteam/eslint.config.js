78e98c325ba5f2da073dcae953be1610741ccc9f
Bragelonne <2338594066@qq.com>
2025-11-29 14:52:23 +0800

    update

--- project-01-anti-fakenews-cn-weareteam/eslint.config.js
+++ project-01-anti-fakenews-cn-weareteam/eslint.config.js
@@ -23,7 +23,10 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'off',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/exhaustive-deps': 'off',
     },
   },
 ])
